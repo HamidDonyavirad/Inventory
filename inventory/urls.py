266350775c3e3a7b23d1e55b4264425a270a6537
HamidDonyavirad <donyavirad76@gmail.com>
2025-06-18 10:15:01 +0330

    feat(urls app): add path login and refresh

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import RegisterView,ProductView, CategoryView, InventoryView, OrderView, OrderItemView
-
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 
 
 urlpatterns = [
     path('register/',RegisterView.as_view(), name='register'),
     path('product/',ProductView.as_view(),name='product'),
-    
+    path('login/',TokenObtainPairView.as_view,name="token_obtain_pair"),
+    path('token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
 ]
