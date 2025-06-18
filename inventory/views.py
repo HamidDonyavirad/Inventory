b81dd661f0d287541d78344f1044eac668427b48
HamidDonyavirad <donyavirad76@gmail.com>
2025-06-18 09:52:45 +0330

    feat(views): add class base views Register and Product

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer,ProductSerializer,CategorySerializer,InventorySerializer,OrderSerializer,OrderItemSerializer
+from rest_framework.response import Response
+from .models import Product,Category,Inventory,Order,OrderItem
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 
 # Create your views here.
+
+class RegisterView(APIView):
+    def post(self,request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'User created'},status=201)
+        return Response(serializer.data, status=400)
+
+class ProductView(APIView):
+    authentication_classes =[JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        product = Product.objects.all()
+        serializer = ProductSerializer(product,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer= ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201) 
+        return Response(serializer.data, status=400) 
+    
+    def put(self,request,pk):
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.data, status=400) 
+    
+    def patch(self,request,pk):
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product,data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.data, status=400)
+    
+    def delete(self,request,pk):
+        product = Product.objects.get(pk=pk)
+        product.delete()
+        return Response(status=204)
+
+class CategoryView(APIView):
+    pass
+
+class InventoryView(APIView):
+    pass
+
+class OrderView(APIView):
+    pass
+
+class OrderItemView(APIView):
+    pass
