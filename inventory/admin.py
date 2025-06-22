b1fcfd9e05d8d90830b0420c904dd43a4e50dca3
HamidDonyavirad <donyavirad76@gmail.com>
2025-06-22 15:31:49 +0330

    feat(admin): adding inventory and category

--- inventory/admin.py
+++ inventory/admin.py
@@ -3,3 +3,5 @@ from .models import Product,Category,Inventory,Order, OrderItem
 # Register your models here.
 
 admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Inventory)
