d0132294d98d097828e773f77bc57da918a86a08
HamidDonyavirad <donyavirad76@gmail.com>
2025-06-23 20:18:22 +0330

    fix(urls):Adding dynamic urls to some paths

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,9 +6,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
     path('register/',RegisterView.as_view(), name='register'),
-    path('product/',ProductView.as_view(),name='product'),
+    path('product/<int:pk>/',ProductView.as_view(),name='product'),
     path('login/',TokenObtainPairView.as_view,name="token_obtain_pair"),
     path('token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
-    path('category/',CategoryView.as_view(),name='category'),
-    path('inventory/',InventoryView.as_view(),name='inventory'),
+    path('category/<int:pk>/',CategoryView.as_view(),name='category'),
+    path('inventory/<int:pk>/',InventoryView.as_view(),name='inventory'),
 ]
