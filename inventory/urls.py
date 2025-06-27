0bee8e929581c932e3d948912637e489ffbe4bc3
HamidDonyavirad <donyavirad76@gmail.com>
2025-06-27 10:46:31 +0330

    refactor(urls):Rename tokenBlacklist to LogoutView

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import RegisterView,ProductView, CategoryView, InventoryView, OrderView, OrderItemView, ProductStockView
-from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,LogoutView
 
 
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('product/',ProductView.as_view(),name='product'),
     path('login/',TokenObtainPairView.as_view(),name="token_obtain_pair"),
     path('token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
+    path('logout/',LogoutView.as_view(),name='logout'),
     path('category/',CategoryView.as_view(),name='category'),
     path('inventory/',InventoryView.as_view(),name='inventory'),
     path('inventory/stock/<int:pk>/', ProductStockView.as_view(), name='product-stock'),
